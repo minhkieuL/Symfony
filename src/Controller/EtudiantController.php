fe19ab03afb19e62366aca562a552f23668a067e
minhkieuL <luongminhkieu@hotmail.fr>
2018-11-13 08:27:47 +0100

    premier import

---
+++ src/Controller/EtudiantController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Etudiant;
+
+class EtudiantController extends AbstractController
+{
+    /*
+     * @Route("/etudiant", name="etudiant")
+     */
+    public function index()
+    {
+     	/* Cette simple instruction permet d'envoyer des informations au navigateur sans passer par une vue.
+        return new Response('<html><body>Salut Les SIO</body></html>');
+        */
+
+         // initialise une variable qui sera exploitée dans la vue
+         $annee = '2019';
+         return $this->render('etudiant/accueil.html.twig', ['pAnnee' => $annee,
+        ]);				         
+    }
+public function ajouterEtudiant(){
+		
+	// récupère le manager d'entités
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // instanciation d'un objet Etudiant
+        $etudiant = new Etudiant();
+        $etudiant->setNom('Potter');
+        $etudiant->setPrenom('Harry');
+        $etudiant->setDateNaissance(new \DateTime('1980-07-31'));
+	$etudiant->setVille('Surrey');
+
+        // Indique à Doctrine de persister l'objet
+        $entityManager->persist($etudiant);
+
+        // Exécue l'instruction sql permettant de persister lobjet, ici un INSERT INTO
+        $entityManager->flush();
+
+        // renvoie vers la vue de consultation de l'étudiant en passant l'objet etudiant en paramètre
+       return $this->render('etudiant/consulter.html.twig', [
+            'etudiant' => $etudiant,]);
+		
+	}
+
+public function consulterEtudiant($id){
+		
+		$etudiant = $this->getDoctrine()
+        ->getRepository(Etudiant::class)
+        ->find($id);
+
+		if (!$etudiant) {
+			throw $this->createNotFoundException(
+            'Aucun etudiant trouvé avec le numéro '.$id
+			);
+		}
+
+		//return new Response('Etudiant : '.$etudiant->getNom());
+		return $this->render('etudiant/consulter.html.twig', [
+            'etudiant' => $etudiant,]);
+	}
+
+public function listerEtudiant(){
+		$repository = $this->getDoctrine()->getRepository(Etudiant::class);
+		$etudiants = $repository->findAll();
+		return $this->render('etudiant/lister.html.twig', [
+            'pEtudiants' => $etudiants,]);	
+		
+	}
+	
+}
---
+++ src/Entity/Etudiant.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EtudiantRepository")
+ */
+class Etudiant
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $dateNaissance;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $rue;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    private $numrue;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $copos;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=true)
+     */
+    private $surnom;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getDateNaissance(): ?\DateTimeInterface
+    {
+        return $this->dateNaissance;
+    }
+
+    public function setDateNaissance(?\DateTimeInterface $dateNaissance): self
+    {
+        $this->dateNaissance = $dateNaissance;
+
+        return $this;
+    }
+
+    public function getRue(): ?string
+    {
+        return $this->rue;
+    }
+
+    public function setRue(?string $rue): self
+    {
+        $this->rue = $rue;
+
+        return $this;
+    }
+
+    public function getNumrue(): ?string
+    {
+        return $this->numrue;
+    }
+
+    public function setNumrue(?string $numrue): self
+    {
+        $this->numrue = $numrue;
+
+        return $this;
+    }
+
+    public function getCopos(): ?int
+    {
+        return $this->copos;
+    }
+
+    public function setCopos(?int $copos): self
+    {
+        $this->copos = $copos;
+
+        return $this;
+    }
+
+    public function getSurnom(): ?string
+    {
+        return $this->surnom;
+    }
+
+    public function setSurnom(?string $surnom): self
+    {
+        $this->surnom = $surnom;
+
+        return $this;
+    }
+
+}
