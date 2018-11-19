88bfa2b9664aee93ce35b3b12929e2935c2cec1f
minhkieuL <luongminhkieu@hotmail.fr>
2018-11-19 10:09:42 +0100

    Tests de vue Etudiant/note note/competences

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -61,4 +61,49 @@ class NoteController extends AbstractController
         }
         }
 	}
+	public function modifierNote($id){
+		
+		//récupération de l'étudiant dont l'id est passé en paramètre
+		$note = $this->getDoctrine()
+        ->getRepository(Note::class)
+        ->find($id);
+
+		if (!$note) {
+			throw $this->createNotFoundException(
+            'Aucun note trouvé avec le numéro '.$id
+			);
+		}
+		else
+		{
+
+
+		// récupération de l'étudiant des griffondor à partir du code de l'étudiant
+		$competences = $this->getDoctrine()
+        ->getRepository(Competences::class)
+        ->findOneByCode('GFD');
+
+		if (!$competences) {
+			throw $this->createNotFoundException(
+            'Aucune competences trouvé avec ce nom'
+			);
+		}
+		else
+		{
+
+		//Affectation de la competences à l'étudiant
+		$note->setEtudiant($competences);
+
+		// persistence de l'objet modifié
+                $entityManager = $this->getDoctrine()->getManager();
+		$entityManager->persist($note);
+		$entityManager->flush();
+
+
+
+		//return new Response('Note : '.$note->getNom());
+		return $this->render('note/consulter.html.twig', [
+            'note' => $note,]);
+        }
+        }
+	}
 }
